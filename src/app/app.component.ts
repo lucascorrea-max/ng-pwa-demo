6081eb6229b8be4f1bec925ce900dbf7ba117e5a
Lucas Corrêa <lucas.correa@maximatech.com.br>
2023-06-27 14:17:25 -0300

    Add platform modal alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Platform } from '@angular/cdk/platform';
 import { Component, OnInit } from '@angular/core';
 import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { filter, map } from 'rxjs';
@@ -10,8 +11,12 @@ import { filter, map } from 'rxjs';
 export class AppComponent implements OnInit {
   isOnline: boolean = false;
   modalVersion: boolean = false;
+  modalPwaEvent: any;
+  modalPwaPlatform: string | undefined;
 
-  constructor(private swUpdate: SwUpdate) {
+  constructor(
+    private platform: Platform,
+    private swUpdate: SwUpdate) {
 
   }
 
@@ -30,6 +35,8 @@ export class AppComponent implements OnInit {
         })
       );
     }
+
+    this.loadModalPwa();
   }
 
   public updateVersion() {
@@ -41,8 +48,34 @@ export class AppComponent implements OnInit {
     this.modalVersion = false;
   }
 
+  public addToHomeScreen(): void {
+    this.modalPwaEvent.prompt();
+    this.modalPwaPlatform = undefined;
+  }
+
+  public closePwa(): void {
+    this.modalPwaPlatform = undefined;
+  }
+
   private updateOnlineStatus(): void {
     this.isOnline = window.navigator.onLine;
     console.info(`isOnline=[${this.isOnline}]`);
+  }
+
+  private loadModalPwa(): void {
+    if (this.platform.ANDROID) {
+      window.addEventListener('beforeinstallprompt', (event: any) => {
+        event.preventDefault();
+        this.modalPwaEvent = event;
+        this.modalPwaPlatform = 'ANDROID';
+      });
+    }
+
+    if (this.platform.IOS && this.platform.SAFARI) {
+      const isInStandaloneMode = ('standalone' in window.navigator) && ((<any> window.navigator)['standalone']);
+      if (!isInStandaloneMode) {
+        this.modalPwaPlatform = 'IOS';
+      }
+    }
   }
 }
