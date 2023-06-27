084591906e69d2209b3daa7d4a0a7a39aaa728c3
Lucas Corrêa <lucas.correa@maximatech.com.br>
2023-06-27 14:03:41 -0300

    Setup pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-pwa-demo';
+export class AppComponent implements OnInit {
+  isOnline: boolean = false;
+  modalVersion: boolean = false;
+
+  constructor(private swUpdate: SwUpdate) {
+
+  }
+
+  ngOnInit(): void {
+    this.updateOnlineStatus();
+
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.pipe(
+        filter((event: any): event is VersionReadyEvent => event.type === 'VERSION_READY'),
+        map((event: any) => {
+          console.info(`Current version: ${event.currentVersion} | Latest version: ${event.latestVersion}`);
+          this.modalVersion = true;
+        })
+      );
+    }
+  }
+
+  public updateVersion() {
+    this.modalVersion = false;
+    window.location.reload();
+  }
+
+  public closeVersion() {
+    this.modalVersion = false;
+  }
+
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+    console.info(`isOnline=[${this.isOnline}]`);
+  }
 }
